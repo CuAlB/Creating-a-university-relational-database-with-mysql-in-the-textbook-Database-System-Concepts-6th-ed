96b6199a49a60c45afbe953ed5a4938881e1a544
CuAlB <116452753+CuAlB@users.noreply.github.com>
2024-05-11 21:40:02 +0800

    Update create table.sql

--- create table.sql
+++ create table.sql
@@ -5,14 +5,14 @@ CREATE TABLE advisor (
   KEY i_ID (i_ID),
   CONSTRAINT advisor_ibfk_1 FOREIGN KEY (i_ID) REFERENCES instructor (ID) ON DELETE SET NULL,
   CONSTRAINT advisor_ibfk_2 FOREIGN KEY (s_ID) REFERENCES student (ID) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE classroom (
   building varchar(15) NOT NULL,
   room_number varchar(7) NOT NULL,
   capacity decimal(4,0) DEFAULT NULL,
   PRIMARY KEY (building,room_number)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE course (
   course_id varchar(8) NOT NULL,
@@ -23,7 +23,7 @@ CREATE TABLE course (
   KEY dept_name (dept_name),
   CONSTRAINT course_ibfk_1 FOREIGN KEY (dept_name) REFERENCES department (dept_name) ON DELETE SET NULL,
   CONSTRAINT course_chk_1 CHECK ((`credits` > 0))
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE department (
   dept_name varchar(20) NOT NULL,
@@ -31,7 +31,7 @@ CREATE TABLE department (
   budget decimal(12,2) DEFAULT NULL,
   PRIMARY KEY (dept_name),
   CONSTRAINT department_chk_1 CHECK ((`budget` > 0))
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE instructor (
   ID varchar(5) NOT NULL,
@@ -42,7 +42,7 @@ CREATE TABLE instructor (
   KEY dept_name (dept_name),
   CONSTRAINT instructor_ibfk_1 FOREIGN KEY (dept_name) REFERENCES department (dept_name) ON DELETE SET NULL,
   CONSTRAINT instructor_chk_1 CHECK ((`salary` > 29000))
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE prereq (
   course_id varchar(8) NOT NULL,
@@ -51,7 +51,7 @@ CREATE TABLE prereq (
   KEY prereq_id (prereq_id),
   CONSTRAINT prereq_ibfk_1 FOREIGN KEY (course_id) REFERENCES course (course_id) ON DELETE CASCADE,
   CONSTRAINT prereq_ibfk_2 FOREIGN KEY (prereq_id) REFERENCES course (course_id)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE section (
   course_id varchar(8) NOT NULL,
@@ -65,7 +65,7 @@ CREATE TABLE section (
   KEY building (building,room_number),
   CONSTRAINT section_ibfk_1 FOREIGN KEY (course_id) REFERENCES course (course_id) ON DELETE CASCADE,
   CONSTRAINT section_ibfk_2 FOREIGN KEY (building, room_number) REFERENCES classroom (building, room_number) ON DELETE SET NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE student (
   ID varchar(5) NOT NULL,
@@ -76,7 +76,7 @@ CREATE TABLE student (
   KEY dept_name (dept_name),
   CONSTRAINT student_ibfk_1 FOREIGN KEY (dept_name) REFERENCES department (dept_name) ON DELETE SET NULL,
   CONSTRAINT student_chk_1 CHECK ((`tot_cred` >= 0))
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE takes (
   ID varchar(5) NOT NULL,
@@ -89,7 +89,7 @@ CREATE TABLE takes (
   KEY course_id (course_id,sec_id,semester,`year`),
   CONSTRAINT takes_ibfk_1 FOREIGN KEY (course_id, sec_id, semester, `year`) REFERENCES section (course_id, sec_id, semester, `year`) ON DELETE CASCADE,
   CONSTRAINT takes_ibfk_2 FOREIGN KEY (ID) REFERENCES student (ID) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE teaches (
   ID varchar(5) NOT NULL,
@@ -101,7 +101,7 @@ CREATE TABLE teaches (
   KEY course_id (course_id,sec_id,semester,`year`),
   CONSTRAINT teaches_ibfk_1 FOREIGN KEY (course_id, sec_id, semester, `year`) REFERENCES section (course_id, sec_id, semester, `year`) ON DELETE CASCADE,
   CONSTRAINT teaches_ibfk_2 FOREIGN KEY (ID) REFERENCES instructor (ID) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE timeslot (
   time_slot_id varchar(4) NOT NULL,
@@ -111,4 +111,4 @@ CREATE TABLE timeslot (
   end_hr decimal(2,0) DEFAULT NULL,
   end_min decimal(2,0) DEFAULT NULL,
   PRIMARY KEY (time_slot_id,`day`,start_hr,start_min)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
